c77d4c32b4d9e7f9db065e3ad0690e55a16e99ea
baronunread <baronunreadts@gmail.com>
2021-07-15 15:49:15 +0200

    remove markdown from messages before normalizing

--- regfilter/regfilter.py
+++ regfilter/regfilter.py
@@ -29,7 +29,8 @@ class Regfilter(commands.Cog):
         self.cache_ignored = []
 
     async def replace(self, msg):
-        nfkd_form = unicodedata.normalize('NFKD', msg)
+        text = discord.utils.remove_markdown(msg)
+        nfkd_form = unicodedata.normalize('NFKD', text)
         return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
     async def updateCache(self, type):
