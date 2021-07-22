b64b177d88433fe6ddd150fe57480ab8e3457c16
baronunread <baronunreadts@gmail.com>
2021-07-22 21:33:23 +0200

    dictionary

--- voicerole/voicerole.py
+++ voicerole/voicerole.py
@@ -1,19 +1,20 @@
 import voicerole
 from redbot.core import commands, Config
 from discord.utils import get
+import json
 
 class Voicerole(commands.Cog):
     """Checks if people have joined the voice chat and gives them the voice chat role."""
     def __init__(self):
         self.config = Config.get_conf(self, identifier = 3434346710410199107115321051023211210)
         default_global = {
-                            "pairs":[]
+                            "pairs":{}
                          }
         self.config.register_global(**default_global)
-        self.cache_voicepairs = []
+        self.cache_voicepairs = {}
 
     async def validate_cache(self):
-        if self.cache_voicepairs == []: 
+        if self.cache_voicepairs == {}: 
             self.cache_voicepairs = await self.config.pairs()
             
     @commands.Cog.listener()
@@ -31,10 +32,10 @@ class Voicerole(commands.Cog):
     async def get_voice(self, voiceChannel):
         await self.validate_cache()
         pairs = self.cache_voicepairs
-        for pair in pairs:
-            if voiceChannel == pair[0]:
-                return pair[1]
-        return None
+        try:
+            return pairs[voiceChannel]
+        except:
+            return None
 
     @commands.group()
     @commands.has_permissions(manage_messages = True)
@@ -45,21 +46,17 @@ class Voicerole(commands.Cog):
     @voicerole.group(name = "add", invoke_without_command = True)
     async def _add(self, ctx, voiceChannelID, voiceRoleID):
         """Adds a voicerole rule. Needs, in order, the voice channel ID and then the voice role ID."""
-        pair = (voiceChannelID, voiceRoleID)
         async with self.config.pairs() as pairs:
-            pairs.append(pair)
+            pairs[voiceChannelID] = voiceRoleID
             self.voicepairs = pairs
         await ctx.send("The new voicerole rule has been added.")
 
     @voicerole.group(name = "delete", invoke_without_command = True)
-    async def _delete(self, ctx, voiceChannelID, voiceRoleID):
-        """Removes a voicerole rule. Needs, in order, the voice channel ID and then the voice role ID."""
+    async def _delete(self, ctx, voiceChannelID):
+        """Removes a voicerole rule. Needs, in order, the voice channel ID."""
         try:
-            pair = (voiceChannelID, voiceRoleID)
             async with self.config.pairs() as pairs:
-                for elem in pairs:
-                    if elem == pair:
-                        pairs.remove(elem)
+                pairs.pop(voiceChannelID)
                 self.cache_voicepairs = pairs
             await ctx.send("Voicerole rule removed successfully.")
         except:
@@ -69,14 +66,11 @@ class Voicerole(commands.Cog):
     async def _list(self, ctx):
         try:    
             await self.validate_cache()
-            list = self.cache_voicepairs
-            if len(list) == 0:
+            dict = self.cache_voicepairs
+            if len(dict) == 0:
                 await ctx.message.author.send("There's nothing in that list.")
                 return
-            prettyList = ""
-            for index, tuple in enumerate(list):
-                work = tuple[0] + " " + tuple[1]
-                prettyList = work + "\n"
+            prettyList = json.dumps(dict)
             await ctx.message.author.send(prettyList)
         except:
             await ctx.send("ERROR: Open your DMs.")
