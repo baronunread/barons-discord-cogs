df3fcbfb09bb56a85f1491e8a7a67702be70ba5b
baronunread <baronunreadts@gmail.com>
2021-07-22 22:09:49 +0200

    fix

--- voicerole/voicerole.py
+++ voicerole/voicerole.py
@@ -24,7 +24,7 @@ class Voicerole(commands.Cog):
         if not before.channel:
             await member.add_roles(role)
         if before.channel and not after.channel:
-            beforeChannel = str(after.channel.id)
+            beforeChannel = str(before.channel.id)
             voice = await self.get_voice(beforeChannel)
             role = get(member.guild.roles, id = voice)
             await member.remove_roles(role)
