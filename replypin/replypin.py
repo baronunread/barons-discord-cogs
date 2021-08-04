a4caf725faf31ed6b67795d161f2dba80372f284
baronunread <baronunreadts@gmail.com>
2021-08-04 21:58:47 +0200

    posting tenor and video, if needed

--- replypin/replypin.py
+++ replypin/replypin.py
@@ -74,7 +74,7 @@ class Replypin(commands.Cog):
             embed.set_image(url = msg.attachments[0].url)
         await channel.send(embed = embed)
         if video or tenor:
-            await ctx.send(video if video else link)
+            await ctx.send(video if video else link if tenor else video + "\n" + link)
         # except:
         #     await ctx.send("Please reply to a post.")
 
