2d32e631af61c713e1c9522b55480205d566b3ad
baronunread <baronunreadts@gmail.com>
2021-08-04 16:40:05 +0200

    testing

--- replypin/replypin.py
+++ replypin/replypin.py
@@ -1,5 +1,6 @@
 from redbot.core import commands
 import discord
+import json
 
 class Replypin(commands.Cog):
     """When called 'pins' the message that was replied to. """
@@ -23,21 +24,33 @@ class Replypin(commands.Cog):
             await ctx.send("Please reply to a post.")
 
     @commands.command()
-    async def test(self, ctx, msg):
-        footer = { "text": ctx.message.created_at.strftime("Posted on the %d/%m/%Y, at %H:%M:%S") }
-        video = { "url": msg }
-        author = { "name": ctx.message.author.display_name, "icon_url": ctx.message.author.avatar_url }
-        data =  {
-                    "title": "Click to jump to message!",
-                    "type": "video",
-                    "description": "test",
-                    "url": ctx.message.jump_url,
-                    "footer": discord.Embed.from_dict(footer),
-                    "video": discord.Embed.from_dict(video),
-                    "author": discord.Embed.from_dict(author)
-                }
-        embed = discord.Embed.from_dict(data)
+    async def test(self, ctx):
+        id = ctx.message.reference.message_id
+        msg = await ctx.fetch_message(id)
+        embed = discord.Embed(title = "Click to jump to message!", url = msg.jump_url, description = msg.clean_content)
+        embed.set_author(name = msg.author.display_name, icon_url = msg.author.avatar_url)
+        if ( len(msg.attachments) > 0 ):
+            embed.add_field(name = "Content attached", value = msg.attachments[0].url)
+            embed.set_image(url = msg.attachments[0].url)
+        embed.set_footer(text = msg.created_at.strftime("Posted on the %d/%m/%Y, at %H:%M:%S"))
         await ctx.send(embed = embed)
+        toDict = discord.Embed.to_dict(embed)
+        json_object = json.dumps(toDict)
+        await ctx.send(json_object)
+        # footer = { "text": ctx.message.created_at.strftime("Posted on the %d/%m/%Y, at %H:%M:%S") }
+        # video = { "url": msg }
+        # author = { "name": ctx.message.author.display_name, "icon_url": ctx.message.author.avatar_url }
+        # data =  {
+        #             "title": "Click to jump to message!",
+        #             "type": "video",
+        #             "description": "test",
+        #             "url": ctx.message.jump_url,
+        #             "footer": discord.Embed.from_dict(footer),
+        #             "video": discord.Embed.from_dict(video),
+        #             "author": discord.Embed.from_dict(author)
+        #         }
+        # embed = discord.Embed.from_dict(data)
+        # await ctx.send(embed = embed)
 
     async def video_or_image(self, msg):
         pass
