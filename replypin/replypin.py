b917c005957d258644d5aee74e45ab789a7f65bf
baronunread <baronunreadts@gmail.com>
2021-08-04 16:47:42 +0200

    testing

--- replypin/replypin.py
+++ replypin/replypin.py
@@ -25,18 +25,7 @@ class Replypin(commands.Cog):
 
     @commands.command()
     async def test(self, ctx):
-        id = ctx.message.reference.message_id
-        msg = await ctx.fetch_message(id)
-        embed = discord.Embed(title = "Click to jump to message!", url = msg.jump_url, description = msg.clean_content)
-        embed.set_author(name = msg.author.display_name, icon_url = msg.author.avatar_url)
-        if ( len(msg.attachments) > 0 ):
-            embed.add_field(name = "Content attached", value = msg.attachments[0].url)
-            embed.set_image(url = msg.attachments[0].url)
-        embed.set_footer(text = msg.created_at.strftime("Posted on the %d/%m/%Y, at %H:%M:%S"))
-        await ctx.send(embed = embed)
-        toDict = discord.Embed.to_dict(embed)
-        json_object = json.dumps(toDict)
-        await ctx.send(json_object)
+
         # footer = { "text": ctx.message.created_at.strftime("Posted on the %d/%m/%Y, at %H:%M:%S") }
         # video = { "url": msg }
         # author = { "name": ctx.message.author.display_name, "icon_url": ctx.message.author.avatar_url }
@@ -49,9 +38,17 @@ class Replypin(commands.Cog):
         #             "video": discord.Embed.from_dict(video),
         #             "author": discord.Embed.from_dict(author)
         #         }
-        # embed = discord.Embed.from_dict(data)
-        # await ctx.send(embed = embed)
-
+        data =  {
+                    "footer": {"text": "Posted on the 04/08/2021, at 14:32:06"},
+                    "author": {"name": "Baron Unread", "icon_url": "https://cdn.discordapp.com/avatars/282971889062772747/0a535cbc3b664762d116615af2179ebe.webp?size=1024"},
+                    "type": "rich", 
+                    "description": "!test https://www.youtube.com/watch?v=rudSWhe_KD0",
+                    "url": "https://discord.com/channels/347702375068467200/769609039977512960/872487066796187679",
+                    "title": "Click to jump to message!"
+                }
+        embed = discord.Embed.from_dict(data)
+        await ctx.send(embed = embed)
+        
     async def video_or_image(self, msg):
         pass
 
