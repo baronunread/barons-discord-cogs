6290bb614de12c0373f75b2b52a6a3bdaac1aa1d
baronunread <baronunreadts@gmail.com>
2021-08-04 19:43:48 +0200

    testing

--- replypin/replypin.py
+++ replypin/replypin.py
@@ -34,32 +34,32 @@ class Replypin(commands.Cog):
     @commands.has_permissions(manage_messages = True)
     async def pinthatshit(self, ctx):
         """'Pins' the post by posting it to another channel."""
-        try:
+        # try:
             #channel = ctx.guild.get_channel(846357308060991558) #Tojo
-            channel = ctx.guild.get_channel(769609039977512960) #Baron
-            id = ctx.message.reference.message_id
-            msg = await ctx.fetch_message(id)
-            links = await self.find_links(msg.clean_content)
-            video = await self.return_video(links[0], msg.attachments[0].url)
-            data =  {
-                        "title": "Click to jump to message!",
-                        "url": msg.jump_url,
-                        "description": msg.clean_content,
-                        "footer": {"text": msg.created_at.strftime("Posted on the %d/%m/%Y, at %H:%M:%S")},
-                        "author": {"name": msg.author.display_name, "icon_url": str(msg.author.avatar_url)}
-                    }
-            embed = discord.Embed.from_dict(data)
-            if video:
-                embed.set_field(name = "Quentin's thought:", value = "There must be a video in that message so I've posted it below this embed!")
-            if links:
-                embed.set_image(url = links[0])    
-            if ( len(msg.attachments) > 0 ):
-                embed.set_image(url = msg.attachments[0].url)
-            await channel.send(embed = embed)
-            if video:
-                await ctx.send(video)
-        except:
-            await ctx.send("Please reply to a post.")
+        channel = ctx.guild.get_channel(769609039977512960) #Baron
+        id = ctx.message.reference.message_id
+        msg = await ctx.fetch_message(id)
+        links = await self.find_links(msg.clean_content)
+        video = await self.return_video(links[0], msg.attachments[0].url)
+        data =  {
+                    "title": "Click to jump to message!",
+                    "url": msg.jump_url,
+                    "description": msg.clean_content,
+                    "footer": {"text": msg.created_at.strftime("Posted on the %d/%m/%Y, at %H:%M:%S")},
+                    "author": {"name": msg.author.display_name, "icon_url": str(msg.author.avatar_url)}
+                }
+        embed = discord.Embed.from_dict(data)
+        if video:
+            embed.set_field(name = "Quentin's thought:", value = "There must be a video in that message so I've posted it below this embed!")
+        if links:
+            embed.set_image(url = links[0])    
+        if ( len(msg.attachments) > 0 ):
+            embed.set_image(url = msg.attachments[0].url)
+        await channel.send(embed = embed)
+        if video:
+            await ctx.send(video)
+        # except:
+        #     await ctx.send("Please reply to a post.")
 
     async def find_links(self, msg):
         links = re.findall(r"(?i)\bhttp[^' ']*", msg)
