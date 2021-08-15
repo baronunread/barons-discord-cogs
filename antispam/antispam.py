a38e3592bd256d1ef086c13dc184f3c162faf55f
baronunread <baronunreadts@gmail.com>
2021-08-15 14:54:49 +0200

    changed message counter

--- antispam/antispam.py
+++ antispam/antispam.py
@@ -125,9 +125,9 @@ class Antispam(commands.Cog):
         await self.config.member(user).previousMessageHash.set(currentMessageHash)
         if deltaTime < 2 or not differentHash:
             messages += 1
-            if messages == 2:
+            if messages == 3:
                 await message.channel.send(user.mention + " stop spamming or you'll be muted.")
-            elif messages >= 4:
+            elif messages >= 5:
                 await self.mute(message.channel, user, role, modChannel, False)
                 return
             await self.config.member(user).messages.set(messages)
