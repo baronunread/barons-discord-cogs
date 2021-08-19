f97b0d408815bdce38643fac4bfd6cc2d509c1e9
baronunread <baronunreadts@gmail.com>
2021-08-19 18:24:49 +0200

    testing

--- replypin/replypin.py
+++ replypin/replypin.py
@@ -83,7 +83,8 @@ class Replypin(commands.Cog):
         for i, link in enumerate(links):
             if "tenor" in link.lower() and i == 0:
                 toRemove = link
-                links[i] = loop.run_until_complete(self.get_tenor(link))
+                task = asyncio.create_task(self.get_tenor(link))
+                links[i] = loop.run_until_complete(task)
                 links.append(toRemove)
             linkType = link.split('/')[-1].split('.')[-1] if "tenor" not in link.lower() else "gif"
             if linkType.lower() not in self.mediaTypesList:
