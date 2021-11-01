5f12e0294d73544fe247833e45c2fc8bd6a9c580
baronunread <baronunreadts@gmail.com>
2021-11-01 02:47:30 +0100

    clean up and minor fixes

--- regfilter/regfilter.py
+++ regfilter/regfilter.py
@@ -1,16 +1,8 @@
 from redbot.core import commands, Config
-from concurrent.futures import ThreadPoolExecutor
-from functools import partial
 import unicodedata
 import discord
 import random
 import re
-import time
-
-def work(content, regex):
-    if regex.search(content):
-        return True
-    return False
 
 class Regfilter(commands.Cog):
     """Uses a REGEX expression to filter bad words.
@@ -55,7 +47,6 @@ class Regfilter(commands.Cog):
         self.cache_names = []
         self.cache_ignore = []
         self.leet_dict = {}
-        self.pool = ThreadPoolExecutor(4)
         self.bot.loop.create_task(self.validate_cache())
 
     async def build_dict(self):
@@ -120,26 +111,6 @@ class Regfilter(commands.Cog):
             await self.compile_cache("ignore")
         if not self.leet_dict:
             await self.build_dict()
-
-    @commands.command()
-    async def test(self, ctx, *, msg):
-        saved = time.perf_counter()
-        for i in range(1000):
-            await self.triggered_filter(msg, self.cache_regex)
-        normalFilterTime = time.perf_counter() - saved
-        saved = time.perf_counter()
-        for i in range(1000):
-            await self.thread_filter(msg)
-        threadFilterTime = time.perf_counter() - saved
-        await ctx.send( "The normal time is {} \n The multithreaded time is {}".format(normalFilterTime,threadFilterTime)    )
-    
-    async def thread_filter(self, msg):
-        process = partial(work, msg)
-        results = self.pool.map(process, self.cache_regex)
-        for result in results:
-            if result: 
-                return True
-        return False
             
     @commands.group()
     @commands.has_permissions(manage_messages = True)
