0d94ecc4cb845773031919b551acfeaf7c628831
baronunread <baronunreadts@gmail.com>
2021-11-01 01:07:11 +0100

    testing

--- regfilter/regfilter.py
+++ regfilter/regfilter.py
@@ -1,10 +1,7 @@
-import sys
-#sys.path.insert(0, regfilter)
 import regfilter
 from redbot.core import commands, Config
 from concurrent.futures import ProcessPoolExecutor
 from functools import partial
-from pathos.pools import ProcessPool
 import unicodedata
 import discord
 import random
@@ -59,7 +56,7 @@ class Regfilter(commands.Cog):
         self.cache_names = []
         self.cache_ignore = []
         self.leet_dict = {}
-        self.pool = ProcessPool(nodes = 9)
+        self.pool = ProcessPoolExecutor()
         self.bot.loop.create_task(self.validate_cache())
 
     async def build_dict(self):
@@ -139,7 +136,7 @@ class Regfilter(commands.Cog):
     
     async def thread_filter(self, msg):
         process = partial(work, msg)
-        results = self.pool.imap(process, self.cache_regex)
+        results = self.pool.map(process, self.cache_regex)
         for result in results:
             if result: 
                 return True
