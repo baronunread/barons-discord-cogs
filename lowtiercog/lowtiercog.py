ae4bd732e68a1f3ee7c10a784861d83deb10da38
baronunread <baronunreadts@gmail.com>
2021-11-05 20:08:08 +0100

    testing

--- lowtiercog/lowtiercog.py
+++ lowtiercog/lowtiercog.py
@@ -10,6 +10,7 @@ class Lowtiercog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.quotes = None
+        self.numQuotes = 0
         self.bot.loop.create_task(self.validate_cache())
     
     @commands.command()
@@ -19,6 +20,7 @@ class Lowtiercog(commands.Cog):
             await ctx.message.attachments[0].save("ltgkeys.json")
             gc = gspread.service_account(filename = "ltgkeys.json")   
             self.quotes = gc.open('ltg').sheet1
+            self.numQuotes = int(self.quotes.acell('F1').value)
             await ctx.send("We have successfully setup everything.")
         except:
             await ctx.send("An error has occured.")
@@ -27,125 +29,61 @@ class Lowtiercog(commands.Cog):
         try:
             gc = gspread.service_account(filename = "ltgkeys.json")   
             self.quotes = gc.open('ltg').sheet1
+            self.numQuotes = int(self.quotes.acell('F1').value)
         except:
             pass
-
-    # async def setup_check(self, ctx):
-    #     if not self.quotes:
-    #         await ctx.send("I haven't been setup yet.")
-
-    # @commands.before_invoke(setup_check)    
+ 
     @commands.group(invoke_without_command = True)
     async def lowtierquote(self, ctx):
         """Base command. Without arguments it posts a random LTG quote."""
-        numberOfQuotes = int(self.quotes.acell('F1').value)
-        selected = random.randint(1, numberOfQuotes)
+        selected = random.randint(1, self.numQuotes)
         quote = self.quotes.cell(selected, 1).value
-        await ctx.send(quote)
+        await ctx.send(quote) 
 
-    @lowtierquote.error
-    async def check_error(self, ctx, error):
-        if not self.quotes:
-            await ctx.send("I haven't been setup yet.")
-        else:
-            await ctx.send("An unexpected error has happened.")
-
-    # @commands.before_invoke(setup_check)
     @lowtierquote.command(name = "show")
     async def _lowtiershow(self, ctx, code):
         """Showcases a specific quote given an ID."""
-        if ( ctx.channel.id == 319638586364395520 or ctx.channel.id == 344539989906030612 ):
-            try:
-                id = int(code)
-                msg = "The given ID wasn't found."
-                with open('quotes.json') as file:
-                    data = json.load(file)
-                quotes = data['quotes']
-                for x in range(len(quotes)):
-                    key = quotes[x]['id']
-                    if ( id == key ):
-                        msg = quotes[x]['msg']
-                        break
-                await ctx.send(msg)
-            except:
-                await ctx.send("Invalid code.")
+        quote = self.quotes.cell(code, 1).value
+        if not quote: quote = "There's no quote associated to that ID."
+        await ctx.send(quote)
 
-    # @commands.before_invoke(setup_check)
     @lowtierquote.command(name = "list")
     async def _lowtierlist(self, ctx):
         """Showcases the current list of quotes. The preview is 50 characters long."""
         output = ""
         messages = []
         user = ctx.message.author
-        with open('quotes.json') as file:
-            data = json.load(file)
-        if ( len(data['quotes']) == 0 ):
-            output = "There are no LTG quotes."
-        for quote in data['quotes']:
-            id = quote['id']
-            msg = quote['msg'][0:50].rstrip().replace('\n', ' ')
-            if ( len(output) >= 1900 ):
+        for i in range(self.numQuotes):
+            msg = self.quotes.cell(i, 1)
+            if len(msg) > 50: msg = msg[0:50].rstrip().replace('\n', ' ') + "..."
+            if len(output) >= 1950:
                 messages.append(output)
                 output = ""
-            output = output + "ID: " + str(id) + " - " + msg
-            if ( len(quote['msg']) > 50 ):
-                output += "..."
-            output += "\n"
-        if ( output != "" ):
-            messages.append(output)
-        try:
-            for output in messages:
-                await user.send(output)
-        except:
-            await ctx.send("ERROR: Please open your DMs.")
+            output += output + "ID: " + str(i) + " - " + msg + "\n"
+        if output: messages.append(output)
+        for output in messages:
+            await user.send(output)
 
-    # @commands.before_invoke(setup_check)
     @lowtierquote.command(name = "add")
     @commands.has_permissions(manage_messages = True)
     async def _lowtieradd(self, ctx, *, msg):
-        """Adds a quote to the JSON file."""
-        id = 0
-        with open('quotes.json') as file:
-            data = json.load(file)
-        try:
-            deleted = data['deleted']
-            id = deleted[0]['id']
-            del deleted[0]
-        except:
-            for quote in data['quotes']:
-                testedId = int(quote['id'])
-                if (id < testedId):
-                    id = testedId
-            id = id + 1
-        add = {
-                "id": id,
-                "msg": msg
-              }
-        data['quotes'].append(add)
-        with open('quotes.json', 'w') as file:
-            json.dump(data, file, indent = 2)
-        await ctx.send("It is done. The ID is %d." %id)
-
-    # @commands.before_invoke(setup_check)
+        """Adds a quote to the list of quotes."""
+        self.numQuotes += 1
+        self.quotes.update_cell(self.numQuotes, 1, msg)
+        await ctx.send("Quote successfully added!")
+        
     @lowtierquote.command(name = "delete")
     @commands.has_permissions(manage_messages = True)
-    async def _lowtierdelete(self, ctx, code):
-        """Removes a quote from the JSON file, given an id."""
-        id = int(code)
-        msg = "The given ID wasn't found."
-        with open('quotes.json') as file:
-            data = json.load(file)
-        quotes = data['quotes']
-        for x in range(len(quotes)):
-            key = quotes[x]['id']
-            if ( id == key ):
-                del quotes[x]
-                add = {
-                        "id": id
-                      }   
-                data['deleted'].append(add)
-                with open('quotes.json', 'w') as file:
-                    json.dump(data, file, indent = 2)
-                msg = "It is done."
-                break
-        await ctx.send(msg)
+    async def _lowtierdelete(self, ctx, *, msg):
+        """Removes a quote from the list of quotes, given the quote."""
+        self.numQuotes -= 1
+        self.quotes.delete_rows(self.quotes.find(msg).row)
+        await ctx.send("Quote successfully deleted!")
+        
+    @lowtierquote.error
+    async def check_error(self, ctx, error):
+        if not self.quotes:
+            await ctx.send("I haven't been setup yet.")
+        else:
+            await ctx.send("An unexpected error has happened.")
+            #HTTPException?
