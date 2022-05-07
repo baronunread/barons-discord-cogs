298f7e09ad13d1dfbd98eb022ff4eb36953bf978
baronunread <baronunreadts@gmail.com>
2022-05-07 15:29:29 +0200

    multi role first version testing

--- antispam/antispam.py
+++ antispam/antispam.py
@@ -1,3 +1,4 @@
+import asyncio
 from redbot.core import commands, Config
 from discord.utils import get
 from discord import Embed 
@@ -34,7 +35,8 @@ class Antispam(commands.Cog):
         self.bot = bot
         self.config = Config.get_conf(self, identifier = 99108971151153265110116105115112)
         default_global = {
-                            "role": None,
+                            "spamRole": None,
+                            "roles": {},
                             "channel": None,
                             "messages": ["has been muted."],
                             "mutes": [],
@@ -42,7 +44,7 @@ class Antispam(commands.Cog):
                          }
         self.config.register_global(**default_global)
         self.config.register_member(warned = False, spamValue = 0, timePrevious = None, previousMessageHash = None, messageList = [], roles = [], secondsOfMute = 0, timeOfMute = None)
-        self.cache_role = None
+        self.cache_roles = {}
         self.cache_channel = None
         self.cache_messages = []
         self.cache_whitelist = []
@@ -56,8 +58,8 @@ class Antispam(commands.Cog):
         await self.start_mute_timers()
     
     async def return_cache(self, type: str):
-        if type == "role":
-            return self.cache_role
+        if type == "roles":
+            return self.cache_roles
         elif type == "channel":
             return self.cache_channel
         elif type == "messages":
@@ -68,8 +70,8 @@ class Antispam(commands.Cog):
     async def update_cache(self, type: str, content = None):
         value = content if content else await self.config.get_raw(type)
         guild = self.cache_guild
-        if type == "role":
-            self.cache_role = get(guild.roles, id = value)
+        if type == "roles":
+            self.cache_roles = {key: get(guild.roles, id = value[key]) for key in value}
         elif type == "channel":
             self.cache_channel = guild.get_channel(value)
         elif type == "messages":
@@ -78,20 +80,20 @@ class Antispam(commands.Cog):
             self.cache_whitelist = value
         
     async def validate_cache(self):
-        if self.cache_role == None: 
-            await self.update_cache("role")
-        if self.cache_channel == None:
+        if not self.cache_roles: 
+            await self.update_cache("roles")
+        if not self.cache_channel:
             await self.update_cache("channel")
-        if self.cache_messages == []:
+        if not self.cache_messages:
             await self.update_cache("messages")
-        if self.cache_whitelist == []:
+        if not self.cache_whitelist:
             await self.update_cache("whitelist")
 
     async def start_mute_timers(self):
         listOfMutes = await self.config.mutes()
         if not listOfMutes: return
         guild = self.cache_guild
-        role = self.cache_role
+        roles = self.cache_roles.values()
         modChannel = self.cache_channel
         message = await modChannel.send("Grabbing local time to restart the muted timers...")
         currentTime = message.created_at.timestamp()
@@ -102,9 +104,11 @@ class Antispam(commands.Cog):
                 user = await guild.fetch_member(user)
             except:
                 continue #if user fetching fails for any reason the user is probably gone
-            if role not in user.roles: #removal of unmuted users from the list and clearing their data
+            check = [role for role in roles if role in user.roles]
+            if not check: #removal of unmuted users from the list and clearing their data
                 await self.config.member(user).clear()
                 continue
+            role = check[0] #if it's here there's at least one item in check
             listOfActualMutes.append(user.id)
             time = await self.config.member(user).secondsOfMute()
             timeOfMute = await self.config.member(user).timeOfMute()
@@ -126,8 +130,11 @@ class Antispam(commands.Cog):
 
     @commands.command(name = "simmerdown")
     @commands.has_permissions(manage_messages = True)
-    async def manual_mute(self, ctx, *, textAndTime :TimeConverter = None):
-        """Manually mutes someone."""
+    async def manual_mute(self, ctx, roleName, *, textAndTime :TimeConverter = None):
+        """Manually mutes someone. If you don't pick a role it will pick the default one"""
+        spamRole = await self.config.spamRole()
+        if not roleName:
+            roleName = spamRole  
         try:
             timeSeconds = textAndTime[0]
             reason = re.sub(r"(?=<)(<...\d+>)|\s{2,}|^\s+", "", discord.utils.escape_mentions(textAndTime[1])).strip()
@@ -135,11 +142,12 @@ class Antispam(commands.Cog):
             timeSeconds = 0
             reason = None
         msgChannel, user = await self.get_context_data(ctx) 
-        role = self.cache_role
+        role = self.cache_roles[roleName]
+        spamRole = self.cache_roles[spamRole]
         modChannel = self.cache_channel
         if user.bot:
             await ctx.send("I can't edit the roles of a bot!")
-        elif role in user.roles:
+        elif role in user.roles or spamRole in user.roles:
             await ctx.send("The user is already muted.")
         else:
             await self.mute(msgChannel, user, role, modChannel, True, timeSeconds, reason, ctx.message.author)
@@ -165,11 +173,13 @@ class Antispam(commands.Cog):
     async def manual_unmute(self, ctx):
         """Manually unmutes someone."""
         msgChannel, user = await self.get_context_data(ctx)
-        role = self.cache_role
+        roles = self.cache_roles.values()
         modChannel = self.cache_channel
+        check = [role for role in roles if role in user.roles]
         if user.bot:
             await ctx.send("I can't edit the roles of a bot!")  
-        elif role in user.roles:
+        elif check:
+            role = check[0]
             listOfMutes = await self.config.mutes()
             if user.id in listOfMutes:
                 listOfMutes.remove(user.id)
@@ -183,10 +193,11 @@ class Antispam(commands.Cog):
     async def timed_mute_info(self, ctx):
         """Checks how much time is left in the muted status."""
         notUsed, user = await self.get_context_data(ctx)
-        role = self.cache_role
+        roles = self.cache_roles.values()
+        check = [role for role in roles if role in user.roles]
         if user.bot:
             await ctx.send("Bots can't be muted so why should I even check up on them?!?")
-        elif role in user.roles:
+        elif check:
             time = await self.config.member(user).secondsOfMute()
             if not time:
                 await ctx.send("The mute is indefinite.")
@@ -230,7 +241,7 @@ class Antispam(commands.Cog):
         """Base command. Check the subcommands."""
         pass
 
-    async def generic_add(self, type, content):
+    async def add_variable(self, type: str, content):
         await self.config.set_raw(type, value = content)
         await self.update_cache(type, content)
 
@@ -239,14 +250,30 @@ class Antispam(commands.Cog):
         list.append(content)
         await self.config.set_raw(type, value = list)
         await self.update_cache(type, list)
+
+    async def add_key(self, type: str, key, id):
+        dict = await self.config.get_raw(type)
+        dict[key] = id
+        await self.config.set_raw(type, value = dict)
+        await self.update_cache(type, dict)
     
-    @antispam.command(name = "addWhitelist")
+    @antispam.command(name = "add")
+    async def add(self, ctx):
+        """Base command. Select what to add."""
+
+    @add.command(name = "role")
+    async def add_role(self, ctx, key, id):
+        """Adds a role to the list of roles."""
+        await self.add_key("roles", key, id)
+        await ctx.send("Successfully added the new role.")
+
+    @add.command(name = "whitelist")
     async def add_whitelist(self, ctx, msg):
         """Adds a channel to the list of whitelisted channels."""
         await self.add_something("whitelist", msg)
         await ctx.send("Successfully added the new ignored channel.")
 
-    @antispam.command(name = "addMuteMessage")
+    @add.command(name = "messages")
     async def add_mute(self, ctx, *, msg):
         """Adds a message that randomly gets sent when muting someone."""
         await self.add_something("messages", msg)
@@ -261,59 +288,88 @@ class Antispam(commands.Cog):
         await self.config.set_raw(type, value = list)
         await self.update_cache(type, list)   
         await ctx.send("Successfully removed the item.")
+
+    async def del_key(self, ctx, type: str, key):
+        dict = await self.config.get_raw(type)
+        dict.pop(key, None)
+        await self.config.set_raw(type, value = dict)
+        await self.update_cache(type, dict)
     
-    @antispam.command(name = "delWhitelist")
+    @antispam.command(name = "delete")
+    async def delete(self, ctx):
+        """Base command. Select what to delete."""
+
+    @delete.command(name = "role")
+    async def add_role(self, ctx, key, id):
+        """Deletes a role from the list of roles."""
+        await self.del_key("roles", key)
+        await ctx.send("Successfully deleted the new role.")
+
+    @delete.command(name = "whitelist")
     async def del_whitelist(self, ctx, *, msg):
         """Removes a channel from the list of whitelisted channels."""
         await self.del_something(ctx, "whitelist", msg)
     
-    @antispam.command(name = "delMuteMessage")
+    @delete.command(name = "messages")
     async def del_mute(self, ctx, *, msg):
         """Removes a message from the list of messages."""
         await self.del_something(ctx, "messages", msg)
 
-    @antispam.command(name = "listWhitelist")
-    async def list_whitelist(self, ctx):
-        """Sends the list of whitelisted channels through DMs"""
-        list = self.cache_whitelist
-        await ctx.message.author.send(f"```{list}```") 
-
-    @antispam.command(name = "listMuteMessage")
-    async def list_mute(self, ctx):
-        """Sends the list of messages through DMs"""
-        list = self.cache_messages
-        await ctx.message.author.send(f"```{list}```")
-       
-    @antispam.command(name = "setup")
-    async def setup(self, ctx, roleID, channelID):
-        """Insert the ID of the role that mutes people and the ID of the mod channel that you'd want to use for the notifications."""
-        await self.generic_add("role", int(roleID))
-        await self.generic_add("channel", int(channelID))
-        await ctx.send("Setup complete.") 
-
-    @antispam.group(name = "edit")
-    async def edit(self, ctx):
-        """Edit the ID of the role or the ID of the mod channel."""
+    @antispam.command(name = "list")
+    async def list_things(self, ctx):
+        """Base command. Select what to list."""
         pass
 
-    @edit.command(name = "role")
-    async def role(self, ctx, roleID):
-        await self.generic_add("role", int(roleID))
-        await ctx.send("Edited the role successfully.")
+    async def list_help(self, ctx, type):
+        list = self.return_cache(type) if (type != "roles") else self.return_cache(type).values()
+        await ctx.message.author.send(f"```{list}```")
 
-    @edit.command(name = "channel")
-    async def channel(self, ctx, channelID):
-        await self.generic_add("channel", int(channelID))
-        await ctx.send("Edited the channel successfully.")
+    @antispam.command(name = "roles")
+    async def list_whitelist(self, ctx):
+        """Sends the list of roles through DMs"""
+        await self.list_help("roles")
     
+    @antispam.command(name = "whitelist")
+    async def list_whitelist(self, ctx):
+        """Sends the list of whitelisted channels through DMs"""
+        await self.list_help("whitelist")
+
+    @antispam.command(name = "messages")
+    async def list_mute(self, ctx):
+        """Sends the list of messages through DMs"""
+        await self.list_help("messages")
+       
+    @antispam.command(name = "setup")
+    async def setup(self, ctx):
+        """Insert the ID of the role that mutes people by default and the ID of the mod channel that you'd want to use for the notifications."""
+        author = ctx.message.author
+        channel = ctx.message.channel
+        def check(m, user):
+            return m.channel == channel and user == author
+        try:
+            await ctx.send("Please send the name of the role that will mute the people with the command by default.")
+            spamRole = await self.bot.wait_for('message', check = check, timeout = 120.0)
+            await ctx.send("Please send the id of the role that will mute the people with the command by default.")
+            spamRoleID = await self.bot.wait_for('message', check = check, timeout = 120.0)
+            await ctx.send("Please send the id of the channel that will be used to notify the server of the mutes.")
+            channelID = await self.bot.wait_for('message', check = check, timeout = 120.0)
+        except asyncio.TimeoutError:
+            await ctx.send("Too much time has passed, I'll be going to sleep...")
+            return    
+        await self.add_variable("spamRole", spamRole)
+        await self.add_key("roles", spamRole, int(spamRoleID))
+        await self.add_variable("channel", int(channelID))
+        await ctx.send("Setup complete.")
+
     @commands.Cog.listener()
     async def on_message(self, message): 
         ctx = await self.bot.get_context(message)
         whitelist = await self.return_cache("whitelist")
         user = message.author
-        if user.bot or ctx.valid or str(ctx.channel.id) in whitelist or not self.cache_role:
+        if user.bot or ctx.valid or str(ctx.channel.id) in whitelist or not self.cache_roles:
             return
-        role = self.cache_role   
+        spamRole = await self.config.spamRole()
+        role = self.cache_roles[spamRole]   
         modChannel = self.cache_channel 
         msgList = await self.config.member(user).messageList()       
         timePrevious = await self.config.member(user).timePrevious() 
@@ -359,7 +415,7 @@ class Antispam(commands.Cog):
         if not selected:
             selected = random.choice(self.cache_messages)
         data =  {
-                    "author": {"name": "MUTED" if not mutedTime else "TIMED MUTE", "icon_url": str(user.avatar_url)}
+                    "author": {"name": "PUNISHED" if not mutedTime else "TIMED PUNISHMENT", "icon_url": str(user.avatar_url)}
                 }
         msgEmbed = Embed.from_dict(data)
         msgEmbed.timestamp = datetime.now(tz = timezone.utc)
@@ -392,18 +448,18 @@ class Antispam(commands.Cog):
     async def unmute(self, user, role, modChannel, msgChannel = None):
         await self.add_roles_and_unmute(user, role)
         msgDict =   {
-                        "author": {"name": "UNMUTED", "icon_url": str(user.avatar_url)},
-                        "description" : f"{user.mention} has been unmuted"                    }
+                        "author": {"name": "FREED", "icon_url": str(user.avatar_url)},
+                        "description" : f"{user.mention} has been freed"                    }
         msgEmbed = Embed.from_dict(msgDict)
         msgEmbed.timestamp = datetime.now(tz = timezone.utc)
         await modChannel.send(embed = msgEmbed)
         if msgChannel:
-            await msgChannel.send(f"{user.mention} you've been unmuted!")
+            await msgChannel.send(f"{user.mention} you've been freed!")
         else: 
             try:
-                await user.send("You've been unmuted!")
+                await user.send("You've been freed!")
             except discord.HTTPException:
-                await modChannel.send(f"I've tried to send a DM to {user.mention} to tell them they've been unmuted but their DMs are closed.")   
+                await modChannel.send(f"I've tried to send a DM to {user.mention} to tell them they've been freed but their DMs are closed.")   
         await self.config.member(user).clear()
         try:    
             timer, = [task for task in all_tasks() if task.get_name() == str(user.id)]
@@ -445,7 +501,7 @@ class Antispam(commands.Cog):
     @timed_mute_info.error
     @purge.error
     async def check_error(self, ctx, error):
-        if not self.cache_role or not self.cache_channel:
+        if not self.cache_roles or not self.cache_channel:
             await ctx.send("I haven't been setup yet.")
         elif isinstance(error.__cause__, AttributeError):
             await ctx.send("I need a reply or a mention to work. For purge I need only the reply.")
